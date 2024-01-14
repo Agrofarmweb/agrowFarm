f22bf79cfe20fd3a7f2b20eec148a2324fa0b16c
Agrofarmweb <156492790+Agrofarmweb@users.noreply.github.com>
2024-01-14 17:13:48 +0530

    Update PricingBox.jsx

--- src/components/Pricing/PricingBox.jsx
+++ src/components/Pricing/PricingBox.jsx
@@ -6,7 +6,7 @@ function PricingBox({ img, price }) {
       <div className="flex flex-col w-1/3 md1000:w-[40rem] bg-white shadow-xl relative min540:w-[100%]">
         <div
           style={{ transition: "all 0.3s" }}
-          className="relative grayscale hover:grayscale-0"
+          className="relative "
         >
           <img src={img} alt="pricing_img" className="w-full h-full" />
           <div className="absolute bg-white text-[20px] font-bold w-[25rem] text-center py-6 text-[#4CAF50] -bottom-[18px] left-0 right-0 mx-auto">
@@ -17,7 +17,7 @@ function PricingBox({ img, price }) {
           {/* price */}
           <p className="text-center text-[55px] font-bold relative py-[10px] ">
             <span className="text-[30px] text-[#6d6d6d] absolute font-normal top-8 -left-[3rem]">
-              Rs.
+            ₹
             </span>
             {price}
             <span className="text-[22px] text-[#6d6d6d] absolute font-thin bottom-[25px] -right-[6rem]">
@@ -27,8 +27,8 @@ function PricingBox({ img, price }) {
           {/* text */}
           <div className="flex flex-col text-[16px] font-medium text-center gap-8 text-[#0a64bc] ">
           <p>Farm Machinery </p>
-          <p>Market Price : <span className="line-through">Rs. 38000 /-</span></p>
-          <p>35% discount</p>
+          <p>Market Price : <span className="line-through"> ₹ 38000 /-</span></p>
+          <p>35% discount . Avail the offer now</p>
 
           </div>
 
